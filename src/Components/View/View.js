7dcafc2f43bb17cdd6029a9ab34f3983e7ba9df4
roshannoonoottil <roshannoonoottil@gmail.com>
2024-10-18 11:57:45 +0530

    refresh data loss issue fixed
    details is stored to the local storage.

--- src/Components/View/View.js
+++ src/Components/View/View.js
@@ -4,47 +4,73 @@ import { PostContext } from '../../store/PostContext';
 import { FirebaseContext } from '../../store/Context';
 
 function View() {
-  const [userDetails, setUserDetails] = useState()
-  const {postDetails} = useContext(PostContext)
-  const {firebase} = useContext(FirebaseContext)
-  useEffect(()=>{
-    const {userId} = postDetails
-    try{firebase.firestore().collection('user').where('id','==',userId).get().then((res)=>{
-      res.forEach(doc=>{
-        console.log(doc.data());
-        
-        setUserDetails(doc.data())
-      })
-    })
-  }catch(err){
-    console.log(err);
-    
+  const [userDetails, setUserDetails] = useState(null);
+  const [storedPostDetails, setStoredPostDetails] = useState(() => {
+    const savedPost = localStorage.getItem('postDetails');
+    return savedPost ? JSON.parse(savedPost) : null;
+  });
+
+  const { postDetails } = useContext(PostContext);
+  const { firebase } = useContext(FirebaseContext);
+
+
+  useEffect(() => {
+    if (postDetails) {
+      localStorage.setItem('postDetails', JSON.stringify(postDetails));
+      setStoredPostDetails(postDetails); 
+    }
+  }, [postDetails]);
+
+
+  useEffect(() => {
+    const post = postDetails || storedPostDetails;
+    if (post) {
+      const { userId } = post;
+      firebase
+        .firestore()
+        .collection('user')
+        .where('id', '==', userId)
+        .get()
+        .then((res) => {
+          res.forEach((doc) => {
+            setUserDetails(doc.data());
+          });
+        })
+        .catch((err) => {
+          console.error('Error fetching user details:', err);
+        });
+    }
+  }, [postDetails, storedPostDetails, firebase]);
+
+
+  if (!postDetails && !storedPostDetails) {
+    return <div>Loading post details...</div>;
   }
-  },[])
-  console.log(userDetails);
-  
+
+  const displayPostDetails = postDetails || storedPostDetails; 
+
   return (
     <div className="viewParentDiv">
       <div className="imageShowDiv">
-        <img
-          src={postDetails.url}
-          alt=""
-        />
+        <img src={displayPostDetails.url} alt="" />
       </div>
       <div className="rightSection">
         <div className="productDetails">
-          <p>&#x20B9; {postDetails.price} </p>
-          <span>{postDetails.name}</span>
-          <p>{postDetails.category}</p>
-          <span>{postDetails.createdAt}</span>
+          <p>&#x20B9; {displayPostDetails.price} </p>
+          <span>{displayPostDetails.name}</span>
+          <p>{displayPostDetails.category}</p>
+          <span>{displayPostDetails.createdAt}</span>
         </div>
-       {userDetails && <div className="contactDetails">
-          <p>Seller details</p>
-          <p>{userDetails.userName}</p>
-          <p>{userDetails.phone}</p>
-        </div>}
+        {userDetails && (
+          <div className="contactDetails">
+            <p>Seller details</p>
+            <p>{userDetails.userName}</p>
+            <p>{userDetails.phone}</p>
+          </div>
+        )}
       </div>
     </div>
   );
 }
+
 export default View;
